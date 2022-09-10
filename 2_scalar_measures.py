1f1f4b7950ab3731a767d7f578e3ed115d40131b
markside10 <113161094+markside10@users.noreply.github.com>
2022-09-10 17:46:46 +0800

    Create 2_scalar_measures.py

---
+++ 2_scalar_measures.py
@@ -0,0 +1,31 @@
+Cost (CURRENCY) = 
+ROUND(
+    CURRENCY([Cost])
+    ,2
+)
+
+Customer Sales LY (COALESCE) = 
+VAR Customer_Sales_LY = 
+CALCULATE(
+    [Customer Sales],
+    DATEADD(
+        'Calendar'[Transaction_Date],
+        -1,
+        Year
+    )
+)
+RETURN
+COALESCE(
+    Customer_Sales_LY,
+    "-"
+)
+
+Total Customers = 
+DISTINCTCOUNT(
+    'Customer Lookup'[customer_id]
+)
+
+Total Employees = 
+COUNTROWS(
+    'Employee Lookup'
+)
