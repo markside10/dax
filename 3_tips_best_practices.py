517388403ff9bdb993554c70852f770c9a841c5b
markside10 <113161094+markside10@users.noreply.github.com>
2022-09-10 17:49:53 +0800

    Update 3_tips_best_practices.py

--- 3_tips_best_practices.py
+++ 3_tips_best_practices.py
@@ -72,3 +72,87 @@ CALCULATE(
 
 Profit = 
 [Customer Sales] - [Cost]
+
+
+
+Date Format (yyyy-mm-dd) = 
+FORMAT(
+    'Calendar'[Transaction_Date],
+    "yyyy-mm-dd"
+) 
+
+
+ISNUMBER = 
+ISTEXT('Calendar'[Week_Desc])
+
+
+Quarter & Year = 
+VAR Q1 = 'Calendar'[Month_ID] IN {1,2,3}
+VAR Q2 = 'Calendar'[Month_ID] IN {4,5,6}
+VAR Q3 = 'Calendar'[Month_ID] IN {7,8,9}
+VAR Q4 = 'Calendar'[Month_ID] IN {10,11,12}
+
+RETURN
+SWITCH(
+    TRUE(),
+    Q1, "Q1" & "-" & 'Calendar'[Year_ID],
+    Q2, "Q2" & "-" & 'Calendar'[Year_ID],
+    Q3, "Q3" & "-" & 'Calendar'[Year_ID],
+    Q4, "Q4" & "-" & 'Calendar'[Year_ID],
+    "-"
+)
+
+
+Year Half = 
+SWITCH(
+    'Calendar'[Month_ID],
+    1, "1H",
+    2, "1H", 
+    3, "1H", 
+    4, "1H", 
+    5, "1H", 
+    6, "1H", 
+    7, "2H",
+    8, "2H",
+    9, "2H",
+    10, "2H",
+    11, "2H",
+    12, "2H",
+    "-"
+)
+
+
+Current Age = 
+FLOOR(    
+    DATEDIFF(
+        'Customer Lookup'[birthdate],
+        TODAY(),
+        DAY
+    ) / 365.25,
+    1
+)
+
+Quantity Sold (CALCULATE) = 
+CALCULATE(  
+    SUM(
+    'Food Inventory'[quantity_sold]
+    )
+)
+
+Quantity Sold (SUM) = 
+SUM(
+    'Food Inventory'[quantity_sold]
+)
+
+
+Time Group = 
+FLOOR(
+    'Sales by Store'[transaction_time],
+    "1:00"
+)
+
+
+Total Revenue = 
+CURRENCY(
+    'Sales by Store'[quantity_sold] * 'Sales by Store'[unit_price]
+) 
